db579dd1fa7873252624e2f770c243355ce68f6a
Adetoba Adedeji <adetoba22@gmail.com>
2021-08-18 00:20:48 +0100

    added printName function

---
+++ printName.js
@@ -0,0 +1,6 @@
+function printMyName(firstname = 'firstname', lastname = 'lastname') {
+  return `Hello, ${firstname} ${lastname} is my name!`;
+}
+
+const myName = printMyName('Adetoba', 'Adedeji');
+console.log(myName);
